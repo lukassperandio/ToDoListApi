d085de0306784c3aa0fd8d9624ab0f8eabaf5363
Lukas Sperandio <emanuelsperandio@gmail.com>
2025-07-24 16:22:47 +0100

    tasks showing in the frontend

--- front-end/to-do-list/src/Pages/MainPage/MainPage.tsx
+++ front-end/to-do-list/src/Pages/MainPage/MainPage.tsx
@@ -1,23 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../Api/AxiosConfig';
+import { Box, Text, VStack } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import './MainPage.css';
+import { IoMdAddCircle } from "react-icons/io";
+
+interface Task {
+  id: number;
+  nameOfTask: string;
+  description: string;
+  endDate: string;
+}
+
 
 function MainPage() {
-  useEffect(() => {
-  console.log("useEffect foi chamado!");
 
-  api.get('/api/v1/tasks/allTasks')
-    .then((response) => {
-      console.log("Tarefas recebidas do backend:", response.data);
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar tarefas:", error);
-    });
-}, []);
+  const [tasks, setTasks] = useState<Task[]>();
+
+  useEffect(() => {
+    api.get('/api/v1/tasks/allTasks')
+      .then((response) => {
+        console.log("Tasks received from the backend:", response.data);
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+      });
+  }, []);
 
   return (
-    <div>
-      <h1>Main Page</h1>
-    </div>
+    <Box className="container">
+      <Text className='add-new-task'>Add new Task <IoMdAddCircle className='add-icon'/></Text>
+      <VStack mt={4} className="task-list" align="stretch">
+        {tasks?.map(task => (
+          <Box
+            key={task.id}
+            className="task-item"
+          >
+            <Text className="task-title">{task.nameOfTask}</Text>
+            <Text className="task-description">{task.description}</Text>
+            <Text className="task-date">{dayjs(task.endDate).format('DD/MM/YYYY')}</Text>
+          </Box>
+        ))}
+      </VStack>
+
+
+    </Box>
   );
 }
 
